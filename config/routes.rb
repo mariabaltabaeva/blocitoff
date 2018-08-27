77cc7094c85d34f4bf2dddc7e2fd6abc64a0754c
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-08-27 18:28:27 -0400

    Can't find user id

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   get 'welcome/index'
 
   get 'welcome/about'
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'users#show'
+
 end
