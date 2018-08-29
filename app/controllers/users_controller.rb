10f940ffb805d18767561e303c6f6fd9bc8803f0
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-08-28 20:52:29 -0400

    US-3:completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
-     @user = User.find(params[:id])
+    id = params[:id].presence || current_user.id
+    @user = User.find(id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  resource :users, only: [:show]
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get 'welcome/index'
 
   get 'welcome/about'
-  
+
   root to: 'users#show'
 
 end
