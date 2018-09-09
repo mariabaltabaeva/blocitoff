2277761d2e534da76abb2e1e91bbbf477ff8886a
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-09-08 21:46:17 -0400

    With after_sign_in_path_for method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180823001409) do
+ActiveRecord::Schema.define(version: 20180908153711) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
